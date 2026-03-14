bf524e0fcf2ad4baf56778d84178bd79e03df7d3
N-1218 <malusarenamrata88@gmail.com>
2026-03-14 11:37:21 +0530

    first java test

--- Java_test/src/Java_test_0005/Test_0005.java
+++ Java_test/src/Java_test_0005/Test_0005.java
@@ -3,7 +3,15 @@ package Java_test_0005;
 public class Test_0005 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num=1;
+		for(int i=1;i<=5;i++) {
+			for (int j=1;j<=i;j++) {
+				System.out.print(num+" ");
+				num++;
+			}
+			
+			System.out.println();
+		}
 
 	}
 
